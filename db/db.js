3b0a75a4d22fc042762444b639bb1c2ba58a2ac6
AakashTripa <aakashtripathi291@gmail.com>
2025-03-18 15:51:22 +0530

    timezone

--- db/db.js
+++ db/db.js
@@ -9,6 +9,7 @@ const db = mysql.createPool({
   port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  timezone: 'IST', 
   waitForConnections: true,
   connectionLimit: 10, 
   queueLimit: 0
